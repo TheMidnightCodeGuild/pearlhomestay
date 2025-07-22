d561162c26e2d6c015dc52453340eea93da45ae9
mayank <mayanktilwankar2355@gmail.com>
2025-07-22 23:36:21 +0530

    done

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Google Analytics */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-KR25WYXMVE"
-        />
-        <script
-          id="gtag-init"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-KR25WYXMVE', { page_path: window.location.pathname });
-            `,
-          }}
-        />
-      </Head>
+      <Head>{/* Google Analytics */}</Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KR25WYXMVE"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KR25WYXMVE', { page_path: window.location.pathname });
+        `}
+      </Script>
       <body className="antialiased">
         <Main />
         <NextScript />
